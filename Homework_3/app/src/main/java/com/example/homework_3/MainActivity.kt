cf453936b783084431a782ffa619e1808109b187
Abbalah19 <keystone1999@yahoo.com>
2024-02-26 15:43:07 -0500

    sub directory test

--- Homework_3/app/src/main/java/com/example/homework_3/MainActivity.kt
+++ Homework_3/app/src/main/java/com/example/homework_3/MainActivity.kt
@@ -9,3 +9,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//cd android_dev
+//
+//# Remove all submodules
+//git submodule status | awk '{print $2}' | xargs -I {} bash -c 'git submodule deinit {} && git rm {} && rm -rf .git/modules/{}'
+//
+//# Add all files to the repository
+//git add .
+//
+//# Commit and push the changes
+//git commit -m "Removed all submodules and added files"
+//git push origin master
